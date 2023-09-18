b6a9342b464729d49d82217d4e4661ecac24ab24
githinjisamson1 <githinjisamson148@gmail.com>
2023-09-18 08:17:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+const game = document.getElementById("game");
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#000000";
+
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "100px";
+
+dodger.style.bottom = "0px";
+
+dodger.style.left = "0px";
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  // if expression is still greater than 0, keep moving dodger to right
+  if (game.clientWidth - (left + dodger.clientWidth) > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
